7164298955c39b945372df3c58399e90c24a3bf5
Satvik <122893966+satmxd@users.noreply.github.com>
2023-12-28 13:17:45 +0530

    Update ascii-converter.py

--- ascii-converter.py
+++ ascii-converter.py
@@ -15,21 +15,11 @@ except OSError as error:
     print(error)
 
 
-
-def clamp(n, min, max): 
-    if n < min: 
-        return min
-    elif n > max: 
-        return max
-    else: 
-        return n 
-
 def img_to_ascii(img, baselength,  bias, contrast, high_detail = False):
     
     #Processing
-    img = Image.open(img)
-    img = ImageEnhance.Contrast(img).enhance(contrast)
-    img = img.convert('L')
+    img = ImageEnhance.Contrast(Image.open(img)).enhance(contrast).convert('L')
+
 
     #Resizing
     x1, y1 = img.size
@@ -38,9 +28,8 @@ def img_to_ascii(img, baselength,  bias, contrast, high_detail = False):
 
     #Converting to ASCII
     ascii_list = list('''$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|(){{}}?-_+~<>i!lI;:,"^`'.'''.format('{}')) if high_detail else list("@&#*?|()?-_+<>i!lI=-:,. ")
-    bias = clamp(bias, 0, 10)
+    bias = 0 if bias < 0 else 10 if bias > 10 else bias #Clamp value between 0 and 10
     bias = bias + 4 if high_detail else bias + 24
-    print(bias)
     px = img.getdata()
     px_img = "".join([ascii_list[i//bias] for i in px])
     ascii_img = "\n".join([px_img[index:(index+baselength)] for index in range(0, len(px_img), baselength)])
@@ -88,7 +77,7 @@ def realtime_ascii():
         
         cv2.imshow('window', frame)
         cv2.imwrite(name, frame)
-        converted_frame = img_to_ascii(name, 180,0, 1.5)
+        converted_frame = img_to_ascii(name, 180,0, 1.2)
         #Realtime Camera feed -> ASCII
         print(converted_frame)
      

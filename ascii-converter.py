c20bec60f30e12fd9b6972e9fca85f6c491cee88
Satvik <122893966+satmxd@users.noreply.github.com>
2024-04-12 16:38:53 +0530

    Update ascii-converter.py

--- ascii-converter.py
+++ ascii-converter.py
@@ -1,12 +1,19 @@
+import sys
+
 from PIL import Image, ImageEnhance
 import cv2
 import time
 import os
 
 dir = os.getcwd()
-ipath = dir+"<Image>.png"
-vpath = dir+"<Video>.mp4"
-vspath = dir+"cache\\"
+print(dir)
+ipath = f"{dir}\\<ImagePath>.png"
+vpath = f"{dir}\\<Video>.mp4"
+vspath = f"{dir}\\cache\\"
+
+w, h = os.get_terminal_size()
+
+
 
 try:
     if not os.path.exists(vspath):
@@ -14,16 +21,16 @@ try:
 except OSError as error:
     print(error)
 
-#TODO-change it to terminal based printing with dynamic size
-def img_to_ascii(img, baselength,  bias, contrast = 1.3, high_detail = False):
-    
+
+def img_to_ascii(img, baselength,  bias, contrast = 1.2, high_detail = False):
+
     #Processing
     img = ImageEnhance.Contrast(Image.open(img)).enhance(contrast).convert('L')
 
 
     #Resizing
     x1, y1 = img.size
-    img = img.resize((baselength, int(y1 * (baselength/(x1*2)))), Image.ANTIALIAS)
+    img = img.resize((baselength, int(y1 * (baselength/(x1*2)))))
 
 
     #Converting to ASCII
@@ -36,54 +43,66 @@ def img_to_ascii(img, baselength,  bias, contrast = 1.3, high_detail = False):
 
     return ascii_img
 
-#print(img_to_ascii(ipath, 200, 0, 2))
 
 def video_to_ascii(vidpath, loop = True):
+    os.system('cls' if os.name == 'nt' else 'clear')
     vid = cv2.VideoCapture(vidpath)
-        
+
     while True:
         try:
             ret, frame = vid.read()
-            if cv2.waitKey(1) & 0xFF == ord('q'): 
+            if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-            
+
             #Generate Cache frame for video read
-            name = dir + 'cache\\cacheframe.jpg'
-            
+            name = f'{dir}\\cache\\cacheframe.jpg'
+
             cv2.imwrite(name, frame)
-            fps = vid.get(cv2.CAP_PROP_FPS)
-            converted_frame = img_to_ascii(name, 75, 4, 1.5)
+            converted_frame = img_to_ascii(name, w, 4, 1.2)
             print(converted_frame)
+            os.system('cls' if os.name == 'nt' else 'clear')
 
             #Delay for smooth Printing
             time.sleep(0.03)
         except:
             if loop:
-                video_to_ascii(1/fps)
+                video_to_ascii(vidpath)
             else:
                 break
-     
-    vid.release() 
-    cv2.destroyAllWindows() 
-                
+
+    vid.release()
+    cv2.destroyAllWindows()
+
 def realtime_ascii():
+    os.system('cls' if os.name == 'nt' else 'clear')
     vid = cv2.VideoCapture(0)
-        
+
     while True:
+        w, h = os.get_terminal_size()
         ret, frame = vid.read()
-        if cv2.waitKey(1) & 0xFF == ord('q'): 
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-        
-        name = dir + 'cache\\cacheframe.jpg'
-        fps = vid.get(cv2.CAP_PROP_FPS)
-        
-        cv2.imshow('window', frame)
+
+        name = f'{dir}\\cache\\cacheframe.jpg'
+
+        #cv2.imshow('window', frame)
         cv2.imwrite(name, frame)
-        converted_frame = img_to_ascii(name, 180,0, 1.2)
+        converted_frame = img_to_ascii(name, w,0, 1.2, True)
         #Realtime Camera feed -> ASCII
         print(converted_frame)
-        time.sleep(1/fps)
-     
-    vid.release() 
-    cv2.destroyAllWindows() 
+        time.sleep(1/30)
+        os.system('cls' if os.name == 'nt' else 'clear')
 
+
+
+    vid.release()
+    cv2.destroyAllWindows()
+
+i = input('V for video, C for camera feed, X to exit: ')
+
+if i == 'V':
+    video_to_ascii(vpath)
+elif i == 'C':
+    realtime_ascii()
+elif i=='X':
+    sys.exit()
